668163a457f54c2bd54ab2c3b00286f24a25820b
basheer mohammad <mdbasheer333@gmail.com>
2023-06-27 21:34:47 +0530

    2706

--- features/core/homepage.js
+++ features/core/homepage.js
@@ -24,3 +24,18 @@ Then('I should also see {int} shopping cart count', function (count) {
 Then('I should also see a paragraph in the middle of the page', function (docString) {
     console.log(docString);
 });
+
+Then('I should see {string} as user and {int} as age and {string} as country', function (name, age, country) {
+    console.log(name, age, country);
+});
+
+Then('I should see below table data', function (dataTable) {
+    let data = dataTable.hashes();
+    for (const each_obj in dataTable.hashes()) {
+        console.log(data[each_obj]);
+    }
+});
+
+When('I enter login with valid {string} and {string} credential details', function (username, password) {
+    console.log("user id is " + username + " and password is " + password);
+  });
