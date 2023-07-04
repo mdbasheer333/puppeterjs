fea9ecf9a11698b101fc8dab80ae64a5f06907e7
basheer mohammad <mdbasheer333@gmail.com>
2023-07-04 08:24:17 +0530

    0407

---
+++ jslearning/demo2.js
@@ -0,0 +1,44 @@
+// var x=10
+// console.log("value of x is " + x);
+// console.log(typeof x);
+
+// x=11.123
+// console.log("value of x is " + x);
+// console.log(typeof x);
+
+// x="basheer"
+// console.log("value of x is " + x);
+// console.log(typeof x);
+
+// x=true
+// console.log("value of x is " + x);
+// console.log(typeof x);
+
+//  var x=undefined;
+// console.log("value of x is " + x);
+// console.log(typeof x);
+
+// var x=null;
+// console.log("value of x is " + x);
+// console.log(typeof x);
+
+// var fname="basheer"
+// var fname=""
+// var fname="basheer jhdaskdh 3y2891738912 %&^%$&^%^"
+// var fname='basheer mohammad';
+// var fname = `basheer`
+// console.log("value of name is " + fname);
+// console.log(typeof fname);
+
+// var person = {
+//     fname:"basheer",
+//     lname:"mohammad",
+//     age:30
+// }
+
+// console.log(person);
+// console.log(typeof person);
+
+// var arr = [1,2,4,3] // arrays 
+// console.log(arr);
+// console.log(typeof arr);
---
+++ jslearning/demo3.js
@@ -0,0 +1,49 @@
+// operators:
+// arithmatic op -- + - * / % 
+
+// var x=10
+// var y=20
+// var z=x%y
+// console.log(z);
+
+// Assignment op = += -= *= /= %=
+// var x=10;
+// x=x+5
+// x+=5;
+// x*=2 // x=x*2
+// console.log(x);
+
+// comparison op == != > < >= <=  === !==
+// console.log(2==2);
+
+// logical operators --> && || !
+// console.log((1==10));
+// console.log(!(1==10));
+
+// increment or decrement  ++ --
+// var x=10
+// x=x+1
+//++x //x++ //x=x+1 or x+=1
+// --x //x-- //x=x-1 or x-=1
+// console.log(x);
+
+// var x=10
+// var y=x++  // post increment y=x and x=x+1
+// var y=++x  // pre increment x=x+1 and x=y
+// console.log("x = " + x +" y = " + y);
+
+// bitwise ope: & | ~ ^
+
+// +
+// console.log(10+20);
+// console.log("java"+"script");
+// console.log("10"+"30");
+
+// var x=new String("10")
+// console.log(typeof x);
+// console.log(x instanceof String);
+
+// if if else else switch
+// for loop, while, for each
+//7-8 am ist; 9.30 - 10.30
+//6-7 8.30-10.30
