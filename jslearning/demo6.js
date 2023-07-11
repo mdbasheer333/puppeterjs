908639edd37aa82b95a692678044537aa9ab9b5b
Basheer Mohammad <mdbasheer333@gmail.com>
2023-07-11 20:50:46 +0530

    1107

--- jslearning/demo6.js
+++ jslearning/demo6.js
@@ -1,4 +1,5 @@
-// var msg="welcome to ejs";
+
+var msg="welcome to ejs AND SELENIUM";
 //var msg='welcome to js';
 // var msg=`welcome to js`;
 
@@ -19,3 +20,31 @@
 // console.log(msg.charAt(1));
 // console.log(msg.indexOf("e"));
 // console.log(msg.lastIndexOf("e"));
+
+//console.log(msg.toUpperCase());
+//console.log(msg.toLowerCase()); //qa QA dev DEV  // YOUR record is success SSN ssn
+
+// console.log("bash"+"eer");
+// console.log("bash".concat("eer"));
+// var fname="bash "
+// var lname="eer";
+// var fullname=fname.concat(lname);
+// console.log(fullname);
+
+//var x="welcome to javascripta";
+//console.log(x.replace('a',"e"));
+//console.log(x.replace(/a/g,"e"));
+
+//var x="welcome to javascript";
+//console.log(x.repeat(3));
+//console.log("a".repeat(10));
+//console.log(x.substring(7));
+//console.log(x.substring(8,12)); //8-11
+// console.log(x);
+// console.log(x.trim());
+// console.log(x.trimEnd());
+// console.log(x.trimStart());
+// var x="welcome;to;javascript";
+// console.log(x.split(";"));
+//console.log(x.search("to"));
+//console.log(x.slice(3));
